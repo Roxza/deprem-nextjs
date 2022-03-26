5b36e3fc0fc2aa3e4c861be26cbdaa623633922f
Roxza <39166624+Roxza@users.noreply.github.com>
2022-03-26 17:12:49 +0300

    API link changed.

--- pages/api/deprem.js
+++ pages/api/deprem.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 export default async function handler(req, res) {
-  const veri = await axios.get("https://api.roxza.me/api/deprem");
+  const veri = await axios.get("https://api.roxza.me/v1/earthquake?location=turkey");
   const data = veri.data;
   const veriler = [];
-  data.kandilli.result.forEach((element) => {
+  data.kandilliData.forEach((element) => {
     const date = `${element.date.substring(8, 10)}.${element.date.substring(
       5,
       7
     )}.${element.date.substring(0, 4)}${element.date.substring(10, 16)}`;
-    const afad = data.afad.data.find((d) => d.time.substring(0, 16) == date);
+    const afad = data.afadData.find((d) => d.time.substring(0, 16) == date);
     veriler.push({
       date: date,
       time: element.date.substring(10),
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
       boylam: element.lat,
       derinlik: element.depth,
       kandilli: `${element.mag}`,
-      afad: afad ? afad.m : "n/a",
+      afad: afad ? afad.m : "N/A",
       bolge: element.lokasyon.replace("-", " ").split("(")[0].split(")"),
       sehir: element.lokasyon.split("(")[1]
         ? element.lokasyon.split("(")[1].split(")")
@@ -26,10 +26,10 @@ export default async function handler(req, res) {
   res.json({
     data: veriler,
     sondeprem: veriler[0],
-    enderindeprem: data.kandilli.result.sort((a, b) => {
+    enderindeprem: data.kandilliData.sort((a, b) => {
       return b.depth - a.depth;
     })[0],
-    enbuyukdeprem: data.kandilli.result.sort((a, b) => {
+    enbuyukdeprem: data.kandilliData.sort((a, b) => {
       return b.mag - a.mag;
     })[0],
   });
